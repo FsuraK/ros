7a1a9cd6756ba3df4acfcb3dcdbe700a3014eb41
Fsurak <2212773877@qq.com>
2022-09-14 11:04:49 +0800

    1

---
+++ guyue_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/Desktop/guyue_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/Desktop/guyue_ws/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/Desktop/guyue_ws/devel;/opt/ros/melodic' != '' else []
---
+++ guyue_ws/build/learing_topic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/Desktop/guyue_ws/src/learing_topic
+# Build directory: /home/ubuntu/Desktop/guyue_ws/build/learing_topic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
