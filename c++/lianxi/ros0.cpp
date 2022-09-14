a98a5ee8f41a1bc0fe0aba012bd627dd9a3ee7c5
Fsurak <2212773877@qq.com>
2022-09-14 10:46:05 +0800

    first ros which is sucessful

---
+++ c++/lianxi/ros0.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+# include <ros/ros.h>
+
+using namespace std;
+
+const int MAX = 4;
+ 
+int main ()
+{
+ cout <<"here"<<endl;
+   return 0;
+}
--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-# include <iostream>
-
-int main()
-{
-	cout<<"l am here"<<endl;
-}
